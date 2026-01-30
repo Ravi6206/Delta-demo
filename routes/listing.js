62b3d5995a19b6c0c27d65b740504d2b22a67f02
Ravi Kumar <ravikumarppl123@gmail.com>
2026-01-30 12:27:44 +0530

    some changes in controllers&route listing

--- routes/listing.js
+++ routes/listing.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
-const listingsController = require("../controllers/Listings.js");
+const listingsController = require("../controllers/listings.js");
 const multer  = require('multer')
 const { storage } = require("../cloudConfig.js");
 const upload = multer({ storage });
@@ -21,7 +21,7 @@ router.route("/:id")
 
 .put(isLoggedIn, isOwner,upload.single('listing[image]'), validateListing, wrapAsync(listingsController.updateListing))
 
-.delete(isLoggedIn, isOwner, wrapAsync(listingsController.destoryListing));
+.delete(isLoggedIn, isOwner, wrapAsync(listingsController.destroyListing));
 
 
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingsController.renderEditForm));
